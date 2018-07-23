f2054f9b5eac9d2b8f03704e49c1debda8d69d7a
Martin Benvenuti <martin.benvenuti@MacBook-Pro-de-It.local>
2018-07-23 02:44:36 -0300

    Hopefully final test

--- ci/aws/start_server.sh
+++ ci/aws/start_server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 cd /var/nodejs
-docker run -d -p 80:80 jointly-node
+docker run -e NODEJS_PORT=${NODEJS_PORT}\
+    -e AWS_DYNAMO_REGION=${AWS_DYNAMO_REGION}\
+    -e AWS_SECRET_ACCES_KEY=${AWS_SECRET_ACCES_KEY}\
+    -e AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID}\
+    -d -p 80:80 jointly-node
